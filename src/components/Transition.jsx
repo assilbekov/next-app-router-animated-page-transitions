cae9dada71ff0ec16cfecc8c9e0cc83828336924
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 04:22:40 -0300

    add first fade in fade out animation

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef, useState } from "react"
 
 
 export const Transition = ({ children }) => {
+  const container = useRef(null);
   const [selectedChildren, setSelectedChildren] = useState(children);
+
+  useGSAP(() => {
+    gsap.to(container.current, { opacity: 0, duration: 1 }).then(() => {
+      setSelectedChildren(children)
+      gsap.to(container.current, { opacity: 1, duration: 1 })
+    })
+  }, [children])
+
   return (
-    <div>
-      sdf
-      {children}
+    <div ref={container}>
+      {selectedChildren}
     </div>
   )
 }
