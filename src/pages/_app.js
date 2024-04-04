332880427298fddd0d2f85220c200ae08c50c441
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 04:40:06 -0300

    add projects transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,12 @@ import { Transition } from "@/components/Transition";
 import { TransitionProvider } from "@/contexts/TransitionContext";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
   return (
     <TransitionProvider>
       <Header />
       <Transition>
-        <Component {...pageProps} />
+        <Component key={router.route} {...pageProps} />
       </Transition>
     </TransitionProvider>
   );
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,37 @@
+import gsap from "gsap";
+import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import { useContext, useRef } from "react";
+import Picture from '../../../public/images/2.jpg'
+import { TransitionContext } from "@/contexts/TransitionContext";
 
 
 export default function Projects() {
+  const {timeline} = useContext(TransitionContext);
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
+
+  useGSAP(() => {
+    const targets = gsap.utils.toArray(["p", imageRef.current])
+    gsap.fromTo(targets, {x: -30, opacity: 0}, {x: 0, opacity: 1, stagger: 0.25})
+    timeline.add(gsap.to(containerRef.current, {opacity: 0}))
+  }, {scope: containerRef})
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-6xl font-bold">Projects</h1>
-      <p className="mt-3 text-2xl">This is the projects page</p>
+    <div ref={containerRef} className='h-[200vh] flex'>
+      <div className="h-[100vh] w-full flex flex-col justify-center items-center gap-5">
+        <p className="text-[5vw]">Projects</p>
+        <p className="max-w-[50%] text-center">Sed ut rhoncus nibh. Cras eleifend tellus a enim sodales, a efficitur odio euismod. Aenean non consequat lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce quis eleifend ipsum, sit amet posuere ligula.</p>
+        <p className="max-w-[50%] text-center">Fusce quis eleifend ipsum, sit amet posuere ligula.</p>
+        <div ref={imageRef} className='relative w-[50%] h-[40vh]'>
+          <Image
+            src={Picture}
+            placeholder='blur'
+            fill
+            style={{objectFit: "cover"}}
+          />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
