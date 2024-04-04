d233c69bfa210324ba3d7f00934f6c9b1362ab97
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 04:19:58 -0300

    add about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">About</h1>
+      <p className="mt-3 text-2xl">This is the about page</p>
+    </div>
+  );
+}
