dfe4ef68f32454cfe915d5c5a959f9c821027d6b
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 04:26:44 -0300

    add images

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,15 @@
 import { Header } from "@/components/Header";
 import { Transition } from "@/components/Transition";
+import { TransitionContext } from "@/contexts/TransitionContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <TransitionContext>
       <Header />
       <Transition>
         <Component {...pageProps} />
       </Transition>
-    </>
+    </TransitionContext>
   );
 }
