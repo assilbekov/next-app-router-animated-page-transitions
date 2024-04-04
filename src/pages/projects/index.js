a6e0e68300b5ae132994cbf905e70261026d8a0d
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 04:20:31 -0300

    add Projects page

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+
+
+export default function Projects() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Projects</h1>
+      <p className="mt-3 text-2xl">This is the projects page</p>
+    </div>
+  );
+}
