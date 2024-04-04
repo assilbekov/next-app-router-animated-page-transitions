686cd5057c855c24ef93c8fe2e775cc8d545a191
assilbekov <askhat.assilbekov@gmail.com>
2024-04-04 04:31:22 -0300

    add About transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,15 @@
 import { Header } from "@/components/Header";
 import { Transition } from "@/components/Transition";
-import { TransitionContext } from "@/contexts/TransitionContext";
+import { TransitionProvider } from "@/contexts/TransitionContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <TransitionContext>
+    <TransitionProvider>
       <Header />
       <Transition>
         <Component {...pageProps} />
       </Transition>
-    </TransitionContext>
+    </TransitionProvider>
   );
 }
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,35 @@
+import { TransitionContext } from "@/contexts/TransitionContext";
+import { useGSAP } from "@gsap/react";
+import Image from "next/image";
+import { useContext, useRef } from "react";
+import Picture from '../../../public/images/3.jpg'
+import gsap from "gsap";
+
 export default function About() {
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
+  const { timeline } = useContext(TransitionContext)
+
+  useGSAP(() => {
+    const targets = gsap.utils.toArray(["p", imageRef.current])
+    gsap.fromTo(targets, { scale: 0.85, opacity: 0 }, { scale: 1, opacity: 1, stagger: 0.25 })
+    timeline.add(gsap.to(containerRef.current, { opacity: 0 }))
+  }, { scope: containerRef })
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-6xl font-bold">About</h1>
-      <p className="mt-3 text-2xl">This is the about page</p>
+    <div ref={containerRef} className='h-[200vh] flex'>
+      <div className="h-[100vh] w-full flex flex-col justify-center items-center gap-5">
+        <p className="text-[5vw]">About</p>
+        <p className="max-w-[50%] text-center">Sed ut rhoncus nibh. Cras eleifend tellus a enim sodales, a efficitur odio euismod. Aenean non consequat lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce quis eleifend ipsum, sit amet posuere ligula.</p>
+        <div ref={imageRef} className='relative w-[50%] h-[40vh]'>
+          <Image
+            src={Picture}
+            placeholder='blur'
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
